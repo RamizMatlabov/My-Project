39c500619a56a15339563510a0b2825747e45374
Ramiz <ramizmatlabov923@gmail.com>
2026-01-12 16:57:33 +0500

    refactor(next.config): migrate from .js to .mjs and update image config
    Consolidate next config into .mjs file format and add remotePatterns configuration for image optimization. The unoptimized flag is set to true to disable default image optimization.

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    unoptimized: true,
   },
 };
 
